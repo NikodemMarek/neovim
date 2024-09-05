14808f86c1fede7e581a86292b6cd7ab32028703
NikodemMarek <nikodemmarek11@gmail.com>
2024-09-05 18:55:57 +0200

    feat(conform-nvim): formatters configured

--- config/plugins/languages/conform-nvim.nix
+++ config/plugins/languages/conform-nvim.nix
@@ -8,9 +8,45 @@
     settings = {
       format_on_save.lsp_format = "first";
       default_format_opts.lsp_format = "first";
+      formatters_by_ft = {
+        nix = [
+          "alejandra"
+        ];
+        javascript = {
+          __unkeyed-1 = "biome";
+          stop_after_first = true;
+        };
+        typescript = {
+          __unkeyed-1 = "biome";
+          stop_after_first = true;
+        };
+        html = {
+          __unkeyed-1 = "biome";
+          stop_after_first = true;
+        };
+        css = {
+          __unkeyed-1 = "biome";
+          stop_after_first = true;
+        };
+        python = ["black"];
+        rust = {
+          __unkeyed-1 = "rustfmt";
+          stop_after_first = true;
+        };
+        "*" = [
+          "trim_whitespace"
+          "squeeze_blanks"
+          "trim_newlines"
+        ];
+        "_" = [
+        ];
+      };
       formatters = {
         alejandra = {
           command = lib.getExe pkgs.alejandra;
+        };
+        biome = {
+          command = lib.getExe pkgs.biome;
         };
         black = {
           command = lib.getExe pkgs.black;
