4029dfdb8c985344947f59f3508afab1e42b12d5
NikodemMarek <nikodemmarek11@gmail.com>
2024-09-06 10:56:18 +0200

    feat(hbac): pin version

--- config/plugins/hbac.nix
+++ config/plugins/hbac.nix
@@ -5,8 +5,8 @@
       src = pkgs.fetchFromGitHub {
         owner = "axkirillov";
         repo = "hbac.nvim";
-        rev = "main";
-        hash = "sha256-JDMP7Nmh5S7eDLd2sJMYrEDZ9W1J/iAtJ8Zuo0rfzEE=";
+        rev = "30dcd055611f6ba6ec37aa2a1c038e211521bd39";
+        hash = "sha256-Jd+KyoP2Ire3vBH8CzLpyWlYBWTH5k8VZAHOSsyaZ2g=";
       };
     })
   ];
