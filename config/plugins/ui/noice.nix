83f90657269ee77a9d695315fe0390d922f43d9d
NikodemMarek <nikodemmarek11@gmail.com>
2024-09-10 16:06:39 +0200

    feat(noice): better command ui and search on the bottom

--- config/plugins/ui/noice.nix
+++ config/plugins/ui/noice.nix
@@ -1,3 +1,39 @@
 {
-  plugins.noice.enable = true;
+  plugins.noice = {
+    enable = true;
+    presets.bottom_search = true;
+    views = {
+      cmdline_popup = {
+        position = {
+          row = 9;
+          col = "50%";
+        };
+        size = {
+          width = 60;
+          height = "auto";
+        };
+      };
+      popupmenu = {
+        relative = "editor";
+        position = {
+          row = 12;
+          col = "50%";
+        };
+        size = {
+          width = 60;
+          height = 10;
+        };
+        border = {
+          style = "rounded";
+          padding = [0 1];
+        };
+        win_options = {
+          winhighlight = {
+            Normal = "Normal";
+            FloatBorder = "DiagnosticInfo";
+          };
+        };
+      };
+    };
+  };
 }
