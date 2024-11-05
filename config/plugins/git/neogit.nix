1bde0bee1babf862c080fbbc1f7ab10f61050444
nm1 <nikodem.marek@softnet.com.pl>
2024-11-05 16:44:28 +0100

    feat(neogit): back to the defauls

--- config/plugins/git/neogit.nix
+++ config/plugins/git/neogit.nix
@@ -1,23 +1,9 @@
 {
-  plugins = {
-    neogit = {
-      enable = true;
-      settings = {
-        kind = "floating";
-        commit_editor = {
-          kind = "floating";
-          show_staged_diff = false;
-        };
-        commit_select_view.kind = "floating";
-        commit_view.kind = "floating";
-        log_view.kind = "floating";
-        rebase_editor.kind = "floating";
-        reflog_view.kind = "floating";
-        merge_editor.kind = "floating";
-        tag_editor.kind = "floating";
-        preview_buffer.kind = "floating";
-        popup.kind = "floating";
-        description_editor.kind = "floating";
+  plugins.neogit = {
+    enable = true;
+    settings = {
+      commit_editor = {
+        staged_diff_split_kind = "auto";
       };
     };
   };
